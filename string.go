648d92db837d3530745203f10880b5282a6c4d71
unknown <wanjuvada@gmail.com>
2023-02-05 01:24:52 +0800

    first commit

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("izzu")
+	fmt.Println("izzu rohman")
+	fmt.Println("izzu rohman firdaus")
+
+	fmt.Println("izzu rohman"[3]) // mengambil karakter ke berapa dari tipe data string
+
+	fmt.Println(len("izzu firdaus")) // mengamvbil panjang tipe data string
+	
+}
---
+++ variable.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "izzu firdaus"
+	fmt.Println(name)
+
+	var panggilan = "bizz"
+	fmt.Println(panggilan)
+
+	negara := "Indonesia"
+	fmt.Println(negara)
+
+	var (
+		umur = 21
+		ttl = "kelayu"
+	)
+
+	fmt.Println(umur)
+	fmt.Println(ttl)
+}
